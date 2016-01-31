e095c62e155b6a5feff036b2a96fab3ebf3c235b
q272654051 <lovejxch@163.com>
2016-01-31 23:12:30 +0800

    1.31_2

---
+++ src/com/black/commons/intreceptor/MyInterceptor.java
@@ -0,0 +1,100 @@
+package com.black.commons.intreceptor;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * 自定义拦截器
+ *
+ * @author tushen
+ * @date Nov 5, 2011
+ */
+public class MyInterceptor extends HandlerInterceptorAdapter {
+
+    /**
+     * 最后执行，可用于释放资源
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+        // TODO Auto-generated method stub
+
+        super.afterCompletion(request, response, handler, ex);
+    }
+
+    /**
+     * 显示视图前执行
+     */
+    @Override
+    public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+        super.postHandle(request, response, handler, modelAndView);
+    }
+
+    /**
+     * 在业务处理器处理请求之前被调用
+     * 如果返回false
+     *     从当前的拦截器往回执行所有拦截器的afterCompletion(),再退出拦截器链
+     *
+     * 如果返回true
+     *    执行下一个拦截器,直到所有的拦截器都执行完毕
+     *    再执行被拦截的Controller
+     *    然后进入拦截器链,
+     *    从最后一个拦截器往回执行所有的postHandle()
+     *    接着再从最后一个拦截器往回执行所有的afterCompletion()
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response, Object handler) throws Exception {
+
+            // 1.获取用户
+            String user_info = (String) request.getSession().getAttribute("user_info");
+            String request_Url = request.getRequestURL().toString();//请求的链接
+            String reg = ".*\\.(?i)(jpg|js|png|css|gif)";
+            //静态资源不进行验证
+            if(request_Url.matches(reg)){
+                return true;
+            }
+            String ResultType ="";
+            if (null == user_info || "".equals(user_info)) {// 如果用户为空(同等于session过期操作)
+                String url = request_Url.substring(request_Url.lastIndexOf("/"), request_Url.length()).replace(".do","");
+                if(url.indexOf("_grzx")!=-1){//个人中心 session过期后操作
+                    ResultType="_grzx";
+                }
+                // 跳到首页
+                if (url.indexOf("index") == -1
+                        && url.indexOf("_noLogin") == -1
+                        && url.indexOf(".") == -1) {// 不需要登录就能进入的页面
+                    Map map = request.getParameterMap();
+                    String params = "";// 参数
+                    if (null != map && map.size() > 0) {
+                        Iterator it = map.keySet().iterator();
+                        while (it.hasNext()) {
+                            String key = "";
+                            String[] value;
+                            key = it.next().toString();
+                            value = (String[]) map.get(key);
+                            params += key + "=" + value[0] + "&";
+                        }
+                        response.sendRedirect(request.getContextPath()+"/back?ResultUrl=" + request.getRequestURL() + "?" + params.substring(0, params.length() - 1));
+                        return false;
+                    } else {
+                        response.sendRedirect(request.getContextPath() + "/back?ResultUrl=" + request.getRequestURL());
+                        return false;
+                    }
+                } else {
+                    return true;
+                }
+            } else {
+                return true;
+            }
+    }
+
+}
